5d0a77000bbc848f67269fffd26a9cd5a84be743
AsmaAlfauri <asmaa.alfauri@gmail.com>
2019-08-04 10:52:54 +0300

    finish the assessment

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function sum(x,y){
   return x+y
 }
 
+
 2) 
 function consoleReturn(x,y){
   console.log(x)
@@ -35,16 +36,36 @@ var object2={
 }
 */
 
-//1) WRITE YOUR CODE UNDER THIS LINE         
+//1) WRITE YOUR CODE UNDER THIS LINE  
+sum(x, y){
+  return x + y
+}
 
-//2) WRITE YOUR CODE UNDER THIS LINE         
+//2) WRITE YOUR CODE UNDER THIS LINE  
+consoleReturn (x, y) {
+  console.log(x)
+  return y
+}
 
 //3) WRITE YOUR CODE UNDER THIS LINE         
+let name = "Alex"
+let age = 25
+let result = `My name is: ${name} and my age is: ${age} `
 
 //4) WRITE YOUR CODE UNDER THIS LINE         
+let food = "Fried Chicken"
+let color = "Blue"
+let object = {
+  food,
+  color
+}
 
 //5) WRITE YOUR CODE UNDER THIS LINE         
-
+let object2 = {
+  multi (a, b) {
+    return a * b
+  }
+}
 
 
 
@@ -74,8 +95,21 @@ Output =>
 
 // WRITE YOUR CODE UNDER THIS LINE
 
+class Computer {
+  constructor (OS, RAM, CPU) { 
+    this.OS = OS,
+      this.RAM = RAM,
+      this.CPU = CPU
+  }
 
-
+   doubleRAM (ram) {
+    this.RAM = ram * 2
+    return `${RAM} ${RAM} ${RAM}`
+  }
+}
+let computer1 =new Computer (Windows,16,I7)
+let computer2 =new Computer (Linux,8,I5)
+let computer3 =new Computer (Mac,4,I3)
 
 
 
@@ -93,36 +127,48 @@ export default class App extends Component {
     title: 'ELIZABETH GREENE',
     todos: ['eat', 'eat eat', 'eact again']
   };
-  changeTitle() {
-    state.title = 'AGGREGOR ZOLDYCK'
+  changeTitle =()=> {
+    this.sitState.title = ({
+      title:'AGGREGOR ZOLDYCK'
+  })
   }
-  render() {
+
+  render(){
     return (
-      <h1>App Component => state.title</h1>
-      <button onClick={this.changeTitle}>Change Title</button>
-      <Tasks tasks={this.todos} changeTitleFromChild={this.changeTitle} />
+    <>
+      <h1> App Component {this.state.title} </h1> 
+      <button onClick = {this.changeTitle} > Change Title </button> 
+      <Tasks tasks = {this.todos} changeTitleFromChild = {this.changeTitle}/>
+      </>
     );
   }
 }
 
+
+
+
 // Tasks Component
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 
 class Tasks extends Component {
+  
   state = {
     day: "Sat"
   };
-  changeDay() {
-    day = 'Sun'
+
+  changeDay(){
+    this.day = 'Sun'
   }
 
   render() {
-    return (
-      <div>
-        <h1>Tasks Component => state.day</h1>
-        <button onClick={this.changeDay}>Change Tasks State</button>
-        <button onClick={changeTitle}>Change App State</button>
-      </div>
+    return ( 
+    <div >
+      <h1> Tasks Component = {this.state.day} </h1> 
+      <button onClick = {this.changeDay} > Change Tasks State </button> 
+      <button onClick = {this.props.changeTitleFromChild } > Change App State </button>
+    </div >
     );
   }
 }
